c612cc570e54254339ac3953e2b56e4b3781a257
kendalHD <ramossergio2004@gmail.com>
2023-08-26 17:48:39 +0200

    eliminar

--- PEuropa/assets/php/Conexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once("configuracionBBDD.php");
-class Conexion {
-    static function Conectar(){
-        $cnx = mysqli_connect(SERVIDOR, USUARIO, PASSWORD, BBDD);
-        return $cnx;
-    }
-}
--- PEuropa/assets/php/configuracionBBDD.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define ("SERVIDOR", "localhost:3307");
-define ("USUARIO", "root");
-define ("PASSWORD", "");
-define ("BBDD", "baseParque");
